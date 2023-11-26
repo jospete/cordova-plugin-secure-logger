90f7006644afdd9c446f7b697e435413828c65b2
Josh Noel <obsidize@gmail.com>
2023-11-26 03:26:49 -0600

    update version sync script for readme changes

--- scripts/version-sync.js
+++ scripts/version-sync.js
@@ -12,6 +12,7 @@ function updateFileVersionRefs(filePath, replaceRegex, replacer) {
 function main() {
     updateFileVersionRefs(`./plugin.xml`, /(<plugin.*version=")([^"]+)(".*)/, `$1${version}$3`);
     updateFileVersionRefs(`./README.md`, /(cordova-plugin-secure-logger.git#)([\d\.]+)/gm, `$1${version}`);
+    updateFileVersionRefs(`./README.md`, /(cordova-plugin-secure-logger@)([\d\.]+)/gm, `$1${version}`);
 }
 
 main();
