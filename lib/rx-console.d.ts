69d035b9c05a147b68736f5e4a017a2a9f9ada62
Josh Noel <obsidize@gmail.com>
2023-11-29 10:27:35 -0600

    v1.0.8

--- lib/rx-console.d.ts
+++ lib/rx-console.d.ts
@@ -1,4 +1,4 @@
-import { type LogEvent } from '@obsidize/rx-console';
+import { type LogEvent, LoggerTransport } from '@obsidize/rx-console';
 /**
  * Converts the given rx-console event to a native event,
  * add adds it to the SecureLogger flush queue.
@@ -14,24 +14,24 @@ export declare function sendRxConsoleEventToNative(ev: LogEvent): void;
  * interval is enabled by default when `SecureLogger`
  * is initialized.
  */
-export declare function enableWebviewListener(): void;
+export declare function enableWebviewListener(transport?: LoggerTransport): void;
 /**
  * Disables event capture proxying for rx-console.
  * No events will be sent to `SecureLogger` from rx-console
  * package after this is called.
  */
-export declare function disableWebviewListener(): void;
+export declare function disableWebviewListener(transport?: LoggerTransport): void;
 /**
  * Activates both rx-console event capture AND the
  * automated event cache flush interval on the plugin.
  * Call this if you need to turn the webview side of the plugin
  * back on after calling `disableWebviewToNative()`.
  */
-export declare function enableWebviewToNative(): void;
+export declare function enableWebviewToNative(transport?: LoggerTransport): void;
 /**
  * Disables both rx-console event capture AND the
  * automated event cache flush interval on the plugin.
  * Call this when you're not running in a cordova or capacitor environment
  * (e.g. vanilla webapp in a browser)
  */
-export declare function disableWebviewToNative(): void;
+export declare function disableWebviewToNative(transport?: LoggerTransport): void;
